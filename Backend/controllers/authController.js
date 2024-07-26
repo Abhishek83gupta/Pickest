a141f76895a883897c2295cda3a7a662879bf780
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-07-26 20:46:28 +0530

    auth

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,56 +1,92 @@
-const User = require("../models/userModel")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+const { generateAccessToken } = require("../Helpers/accessToken");
+const { generateRefreshToken } = require("./refreshToken");
 
+// Signup user
+const signup = async (req, res) => {
+  const { username, email, password, accountType } = req.body;
 
-
-
-const signup = async (req,res)=>{
-   const {username, email, password, accountType} = req.body;
-
-   try {
-
+  try {
     // User is already exist or not
-    let user = await User.findOne({username})
-    if(user){
-        return res
+    let user = await User.findOne({ username });
+    if (user) {
+      return res
         .status(400)
-        .json({success: false, message: "Username already in use"})
+        .json({ success: false, message: "Username already in use" });
     }
-    
+
+    // Creating user
     // hashing password
     const salt = await bcrypt.genSalt(10);
-    const securePassword = await bcrypt.hash(password, salt)
-    
-    user = new User({
-        username,
-        email,
-        password:securePassword,
-        accountType
-    })
+    const securePassword = await bcrypt.hash(password, salt);
 
-    // await user.save()       // Here complusory
-    
-    // const newUser = await User.create({username, email, password:securePassword, accountType})
+    // user = new User({
+    //   username,
+    //   email,
+    //   password: securePassword,
+    //   accountType,
+    // });
+
+    // await user.save(); // Here complusory
+
+    const newUser = await User.create({username, email, password:securePassword, accountType})
     // await newUser.save()    // Here not complusory
-
     return res
-    .status(201)
-    .json({ success: true, message: "User created succesfully"})
+      .status(201)
+      .json({ success: true, message: "User created succesfully" });
 
-   } catch (error) {
-     return res.status(500).json({success: false, message: error.message})
-   }
+
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 
-const login = async ()=>{
-  
+
+// login user
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    //user is available
+    let user = await User.findOne({ email });             // change user here then change all the user
+    if (!user) {
+      return res.status(400).json({ success: true, message: "please Signup" });
+    }
+
+    //if user available
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword)
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+
+    const data = {
+        id: user.id,
+        accountType : user.accountType,
+        author : user.username,
+    }
+
+    const accessToken = generateAccessToken(data)
+    const refreshToken = generateRefreshToken(data)
+ 
+    return res.status(200).json({
+        success:true,
+        message:"Login successful",
+        accessToken,
+        refreshToken,
+        role:user.accountType,
+        author:user.username
+    })
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
 };
 
-
-module.exports = { 
-    login,
-    signup
-}
+module.exports = {
+  login,
+  signup,
+};
