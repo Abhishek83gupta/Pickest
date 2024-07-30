5656e378814c66a4dc2346620e3ed5035c627a3f
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-07-30 21:22:33 +0530

    state added

--- Frontend/store/store.js
+++ Frontend/store/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authSlice from "./slices/authSlice.js"
-
+import navSlice from "./slices/navSlice.js"
 
 export const store = configureStore({
     reducer :{
        " Key to identify slice " : "slice file",
        // shakkar : shakkarSlice,
        auth : authSlice,
+       nav : navSlice
     }
 })
