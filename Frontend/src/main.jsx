407236a278bba9e2f7f148bcdb40d90c8f63af20
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-08-13 23:42:17 +0530

    protected routes added

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,49 +1,66 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Home, SellerDashboard , Login, Signup, BuyerDashboard} from './Components/index.js'
-import { Toaster } from 'react-hot-toast'
-import { Provider } from 'react-redux'
-import { store } from '../store/store.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Home,
+  SellerDashboard,
+  Login,
+  Signup,
+  BuyerDashboard,
+} from "./Components/index.js";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { store } from "../store/store.js";
+import ProtectedRoute from "./Components/ProtectedRoutes.jsx";
 
-const router =createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:'/',
-    element:<App/>,
-    children:[
+    path: "/",
+    element: <App />,
+    children: [
       {
-        path:'/',
-        element:<Home/>
+        path: "/",
+        element: <Home />,
       },
       {
-        path:'/login',
-        element:<Login/>
+        path: "/login",
+        // element: <ProtectedRoute children={<Login/>} requireAuth={false}/>
+        element: <ProtectedRoute requireAuth={false}>
+                      <Login/>       
+                 </ProtectedRoute>
       },
       {
-        path:'/Signup',
-        element:<Signup/>
+        path: "/Signup",
+        // element: <ProtectedRoute children={<Signup/>} requireAuth={false}/>
+        element: <ProtectedRoute requireAuth={false}>
+                    <Signup/>       
+                 </ProtectedRoute>
       },
       {
-        path:'/seller/profile',
-        element:<SellerDashboard/>
+        path: "/seller/profile",
+        // element: <ProtectedRoute children={<SellerDashboard/>}/>
+        element: <ProtectedRoute >
+                    <SellerDashboard/>       
+                 </ProtectedRoute>
       },
       {
-        path:'/buyer/profile',
-        element:<BuyerDashboard/>
-      }
-    ]
-  }
-])
+        path: "/buyer/profile",
+        // element: <ProtectedRoute children={<BuyerDashboard/>}/>
+        element: <ProtectedRoute >
+                   <BuyerDashboard/>       
+                </ProtectedRoute>
+      },
+    ],
+  },
+]);
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-       <RouterProvider router={router}/>
-       <Toaster/>
+      <RouterProvider router={router} />
+      <Toaster />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
