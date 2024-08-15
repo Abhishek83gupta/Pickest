6c3d95e52a8950be814024afbccc277ed0e8e788
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-08-15 20:03:30 +0530

    switch profile added

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router()
 
-const { login, signup } = require("../controllers/authController");
+const { login, signup, refresh, switchProfile } = require("../controllers/authController");
+const { verifyToken } = require("../middlewares/verifyToken")
 
 
 router.post("/login",login)
 router.post("/signup",signup)
-
-
+router.get("/refresh", refresh)
+router.get('/switch', verifyToken, switchProfile)
 
 module.exports = router;
