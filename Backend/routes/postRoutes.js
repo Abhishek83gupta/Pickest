6fafac72f50dd7f1946684c95356d949399f3aa7
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-09-13 16:03:24 +0530

    search functionality added

--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const { verifyToken } = require("../middlewares/verifyToken");
 const { 
      createPost,
-     getAllPost, 
+     getAllPosts, 
      getMyPosts,
      deletePost,
      searchPost,
@@ -14,10 +14,10 @@ const {
    } = require("../controllers/postController");
 
 router.post("/post/create", verifyToken, createPost);
-router.get("/post/getAll", getAllPost)
+router.get("/post/getAll", getAllPosts)
 router.get("/post/myPosts", verifyToken, getMyPosts)
 router.delete("/post/delete/:id", verifyToken, deletePost)
-router.get("/posts/search", searchPost)
+router.get("/post/search", searchPost)
 router.put("/post/addToFavourites/:postId",verifyToken,addToFavourites )
 router.put("/post/addToFavourites/:postId",verifyToken,removeFromFavourites )
 router.get("/posts/favourites",verifyToken, getFavourites)
