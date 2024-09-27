4860a706da7591c1f32a8f7476b5e7219bc4b5d6
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-09-27 19:29:09 +0530

    feat : Buyer DashBoard

--- Frontend/src/pages/BuyerDashboard.jsx
+++ Frontend/src/pages/BuyerDashboard.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { Analytics, DashBoardSidebar, PhotoManagement } from "../Components/index.js";
+import Orders from "../Components/Orders.jsx";
+import { useSelector } from "react-redux";
+import PhotosPurchased from "../Components/buyer/PhotosPurchased.jsx"
 
 const BuyerDashboard = () => {
+  
+  const tab = useSelector((state)=>state.nav.tab);
+
   return (
-    <div>
-      Buyer dashboard
+    <div className="flex flex-col sm:flex-row">
+      <DashBoardSidebar />
+      <div>
+        {/* We will change the pages through switch case here */}
+        {(() => {
+          switch (tab) {
+            case "photos-management" :
+              return <PhotosPurchased/>;
+            case "analytics" :
+              return <Analytics/>;
+            case "orders" :  
+              return <Orders/>
+
+
+            default:
+              return <PhotosPurchased/>;
+          }
+        })()}
+      </div>
     </div>
-  )
+  );
 }
 
 export default BuyerDashboard
